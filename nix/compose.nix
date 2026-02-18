44101a68c640c0927fc5d023b426741852ad8cba
DevJoshi29 <etlocomotin@gmail.com>
2026-02-18 21:42:10 +0000

    use pkgs.gradle_9 for backend dev process

--- nix/compose.nix
+++ nix/compose.nix
@@ -22,7 +22,7 @@
         settings.processes = {
           backend.command = ''
             cd "$FLAKE_ROOT/backend" &&
-            ${lib.getExe pkgs.gradle} bootRun --rerun-tasks
+            ${lib.getExe pkgs.gradle_9} bootRun --rerun-tasks
           '';
           frontend.command = ''
             cd "$FLAKE_ROOT/frontend" &&

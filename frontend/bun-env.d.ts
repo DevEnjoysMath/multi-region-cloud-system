c99a3457f19b4f633856ec35b4c99c195e67b451
dschwer <schwerd@tcd.ie>
2026-02-24 10:17:41 +0000

    render image

--- frontend/bun-env.d.ts
+++ frontend/bun-env.d.ts
@@ -8,6 +8,12 @@ declare module "*.svg" {
   export = path;
 }
 
+declare module "*.png" 
+{
+  const src: string;
+  export default src;
+}
+
 declare module "*.module.css" {
   /**
    * A record of class names to their corresponding CSS module classes
